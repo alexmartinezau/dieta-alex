faeb99c0750e41a4eeb7345d88f8762533f59c84
alexmartinezau <alex@freewatts.com.au>
2026-07-20 15:40:11 -0600

    Update offline cache assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dieta-alex-v1.0.1";
-const ASSETS=["./","./index.html","./data.js","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE="dieta-alex-v1.0.2";
+const ASSETS=["./","./index.html","./data.js","./app.js","./manifest.webmanifest","./icons/icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data?.type==="SKIP_WAITING")self.skipWaiting()});
