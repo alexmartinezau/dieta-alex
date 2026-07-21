79ca8d91a4e360b82eedea86b2fe35eb4e8d1937
alexmartinezau <alex@freewatts.com.au>
2026-07-20 18:32:05 -0600

    Refresh cached PWA assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dieta-alex-v1.2.0";
-const ASSETS=["./","./index.html","./data.js","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE="dieta-alex-v1.2.1";
+const ASSETS=["./","./index.html","./data.js?v=1.2.1","./app.js?v=1.2.1","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data?.type==="SKIP_WAITING")self.skipWaiting()});
